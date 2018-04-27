758c3edc4320e14ccbf4ffcfa3ce2beaba3961c3
Weekend Master <weekend@super.com>
2018-04-28 00:11:43 +0300

    simple button handler

--- app/src/main/java/com/example/app/app/MainActivity.java
+++ app/src/main/java/com/example/app/app/MainActivity.java
@@ -2,12 +2,22 @@ package com.example.app.app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText editText_userEmail;
+    private EditText editText_userPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onLoginClick(View view) {
+        Log.i("onLoginClick()", "clicking");
     }
 }
